e585b0dfca774279ee71eca0acf282236d68b5b1
Giovanni Costa <cgiovanni1996@gmail.com>
2023-08-10 16:34:15 +0200

    :sparkles: sign-out component

---
+++ components/auth/sign-out.tsx
@@ -0,0 +1,26 @@
+import Image from "next/image";
+import { SignedIn, SignOutButton } from "@clerk/nextjs";
+
+import { svgDimensions } from "@/constants";
+
+type Props = {
+  withLabel?: boolean;
+  onClick?: () => void;
+};
+
+export const SignOut = ({ withLabel, onClick }: Props) => {
+  return (
+    <SignedIn>
+      <SignOutButton signOutCallback={onClick && onClick}>
+        <div className={`flex cursor-pointer ${withLabel && "gap-4 p-4"}`}>
+          <Image
+            src="/assets/logout.svg"
+            alt="logout"
+            {...svgDimensions.default}
+          />
+          {withLabel && <p className="text-light-2 max-lg:hidden">Logout</p>}
+        </div>
+      </SignOutButton>
+    </SignedIn>
+  );
+};
