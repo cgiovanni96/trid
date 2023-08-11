0e39a4c6750370b6a9c2ac89677698c4fad67ec8
Giovanni Costa <cgiovanni1996@gmail.com>
2023-08-11 09:59:27 +0200

    :heavy_plus_sign: added shadcn/ui form component

--- components/ui/index.ts
+++ components/ui/index.ts
@@ -1,1 +1,6 @@
 export { SideLinks } from "./side-links.ui";
+export * as Form from "./form";
+export { Input } from "./input";
+export { Button } from "./button";
+export { Label } from "./label";
+export { Textarea } from "./textarea";
